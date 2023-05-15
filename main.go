8767533f89d1fce2e6bfa69b75e2333b96695c79
juliussiregar <juliussiregar1011@gmail.com>
2023-05-15 16:39:43 +0700

    acount service app project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"database/sql"
+	// "fakhry/rawsql/controllers"
+	// "fakhry/rawsql/entities"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {}
