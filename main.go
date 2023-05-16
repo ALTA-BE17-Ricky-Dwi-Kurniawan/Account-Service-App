e50a7e214f0a0e2bb766424f0d6a46d29a4c9a3e
juliussiregar <juliussiregar1011@gmail.com>
2023-05-16 10:34:42 +0700

    silahkan mas ricky

--- main.go
+++ main.go
@@ -5,39 +5,52 @@ import (
 	// "fakhry/rawsql/controllers"
 	// "fakhry/rawsql/entities"
 
-	"account_service_app_project/database"
+	// "account_service_app_project/database"
+	// "account_service_app_project/user_account"
 	"account_service_app_project/user_account"
+	"fmt"
+
+	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	"os"
 )
 
 
 
 func main() {
-	conn := database.InitSQL()
-	mdl := user_account.UserModel{}
-	mdl.SetSQLConnection(conn)
+	fmt.Println("test")
+	var db_connection = os.Getenv("dbconnection")
+	db, err := sql.Open("mysql", db_connection)
+	// fmt.Println("tidak berhasil", err.Error())
+	if err != nil {
+		fmt.Println(err)
+		
+	}
+
+	if db.Ping() != nil {
+		fmt.Println("tidak berhasil")
+		fmt.Println(db.Ping().Error())
+	}
+
+// 	conn := database.InitSQL()
+	
+// 	mdl := user_account.UserModel{}
+	
+// 	mdl.SetSQLConnection(conn)
+	fmt.Println("berhasil")
+	var name user_account.User_account
+
+	
+	errorRegisterUser:= user_account.RegisterUser(db, name)
+	fmt.Scanln("Nama Anda:", name)
+	if errorRegisterUser != nil {
+		fmt.Println("Nama anda tidak valid")
+		fmt.Println(errorRegisterUser.Error())
+}
 }
 
-// func registerUser(db *sql.DB, name string, password string) {
 
-// 	query := "select username from user where username = ?"
-// 	row := db.QueryRow(query, name)
 
-// 	var existingUsername string
 
-// 	err := row.Scan(&existingUsername)
-// 	if err == nil {
-// 		fmt.Println("name", existingUsername, "sudah terdaftar.")
-// 		return
-// 	}
-
-// 	insertQuery := "insert into user_account (name, password), values (?, ?)"
-// 	_, err = db.Exec(insertQuery, name, password)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Println("Registerasi berhasil. Username:", name)
-
-// }
--- user_account/entities.go
+++ user_account/entities.go
@@ -5,19 +5,20 @@ import (
 	"fmt"
 )
 
-type user_account struct {
-	id           int
-	name         string
-	phone_number int
-	gender       string
-	password     string
+type User_account struct {
+	Id           int
+	Name         string
+	Phone_number int
+	Gender       bool
+	Password     string
 }
-
-func RegisterUser(db *sql.DB, user user_account) error {
-	insertQuery := "insert into users (name, phone_number, gender, password) values (?, ?, ?, ?)"
-	_, err := db.Exec(insertQuery, user.name, user.phone_number, user.gender, user.password)
+func RegisterUser(db *sql.DB, user User_account) error {
+	insertquery := "insert into user_account (Name, Phone_number, Gender, Password) values (?, ?, ?, ?)"
+	_, err := db.Exec(insertquery, user.Name, user.Phone_number, user.Gender, user.Password)
 	if err != nil {
-		return fmt.Errorf("failed to create user: %v", err)
+		return fmt.Errorf("failed to create user account: %v", err)
 	}
 	return nil
 }
+
+
